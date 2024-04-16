48bef2bfc0654c37288d7f3c52acf339c51ead55
Murilo Nascimento <murilobsi2018@gmail.com>
2024-04-15 22:04:45 -0300

    :sparkles: feat(Tradução): Cria arquivo de tradução. Re #1.

---
+++ lang/pt-br/messages.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'rememberMe' => 'Lembrar de mim',
+    'login' => 'Entrar',
+    'logOut' => 'Sair',
+    'forgotPassword?' => 'Esqueceu a senha?',
+    'password' => 'Senha',
+    'email' => 'Email',
+    'dashboard' => 'Início',
+    'failed' => 'Essas credenciais não correspondem aos nossos registros.',
+    'passwordIncorrect' => 'A senha fornecida está incorreta.',
+    'throttle' => 'Muitas tentativas de login. Por favor, tente novamente em :seconds segundos.',
+    'profile' => 'Perfil',
+    'profileInformation' => 'Informação do perfil',
+    'name' => 'Nome',
+    'save' => 'Salvar',
+    'saved' => 'Salvo.',
+    'deleteAccount' => 'Deletar conta',
+];
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,21 +15,21 @@
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <x-input-label for="password" :value="__('messages.password')" />
 
             <x-text-input id="password" class="block mt-1 w-full"
                             type="password"
                             name="password"
                             required autocomplete="current-password" />
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+            <x-input-error :messages="$errors->get('messages.passwordIncorrect')" class="mt-2" />
         </div>
 
         <!-- Remember Me -->
         <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
                 <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="ml-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
+                <span class="ml-2 text-sm text-gray-600 dark:text-gray-400">{{ __('messages.rememberMe') }}</span>
             </label>
         </div>
 
@@ -37,7 +37,7 @@
         <div class="flex items-center justify-start mt-4">
             @if (Route::has('magic-login-create'))
                 <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('magic-login-create') }}">
-                    {{ __('Login visitante') }}
+                    {{ __('Página do visitante') }}
                 </a>
             @endif
         </div>
@@ -45,12 +45,12 @@
         <div class="flex items-center justify-end">
             @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
+                    {{ __('messages.forgotPassword?') }}
                 </a>
             @endif
 
             <x-primary-button class="ml-3">
-                {{ __('Log in') }}
+                {{ __('messages.login') }}
             </x-primary-button>
         </div>
     </form>
