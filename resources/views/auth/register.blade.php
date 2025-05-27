cdf077c84b4e18e877c468893f908302937d0006
Murilo Nascimento <murilobsi2018@gmail.com>
2025-05-27 11:18:01 -0300

    :cyclone: feat(Colaboradores): Ajuste de style.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,97 +11,39 @@
                 <div class="p-6 bg-white dark:bg-gray-800 border-b border-gray-200 dark:border-gray-700">
                     <form method="POST" action="{{ route('register.store') }}">
                         @csrf
-                        {{-- <div class="grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2"> --}}
-                        <div class="grid-cols-4 gap-2">
-
+                        <div class="flex flex-col sm:flex-row gap-2">
                             <!-- Name -->
-                            <div>
+                            <div class="flex-1">
                                 <x-input-label for="name" :value="__('Nome')" />
                                 <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
                                 <x-input-error :messages="$errors->get('name')" class="mt-2" />
                             </div>
 
                             <!-- Email Address -->
-                            <div>
+                            <div class="flex-1">
                                 <x-input-label for="email" :value="__('Email')" />
                                 <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
                                 <x-input-error :messages="$errors->get('email')" class="mt-2" />
                             </div>
 
                             <!-- Password -->
-                            <div>
+                            <div class="flex-1">
                                 <x-input-label for="password" :value="__('Senha')" />
-
                                 <x-text-input id="password" class="block mt-1 w-full"
                                                 type="password"
                                                 name="password"
                                                 required autocomplete="new-password" />
-
                                 <x-input-error :messages="$errors->get('password')" class="mt-2" />
                             </div>
 
                             <!-- Confirm Password -->
-                            <div>
+                            <div class="flex-1">
                                 <x-input-label for="password_confirmation" :value="__('Confirmar senha')" />
-
                                 <x-text-input id="password_confirmation" class="block mt-1 w-full"
                                                 type="password"
                                                 name="password_confirmation" required autocomplete="new-password" />
-
                                 <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
                             </div>
-
-                        </div>
-
-    <x-slot name="header">
-        <x-views-header title="{{__('messages.colaboradores')}}" />
-    </x-slot>
-
-    <div class="py-6">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            @include('errors._alerts')
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white dark:bg-gray-800 border-b border-gray-200 dark:border-gray-700">
-                    <form method="POST" action="{{ route('register.store') }}">
-                        @csrf
-                        <div class="grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
-                            <!-- Name -->
-                            <div>
-                                <x-input-label for="name" :value="__('Nome')" />
-                                <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-                                <x-input-error :messages="$errors->get('name')" class="mt-2" />
-                            </div>
-
-                            <!-- Email Address -->
-                            <div>
-                                <x-input-label for="email" :value="__('Email')" />
-                                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-                                <x-input-error :messages="$errors->get('email')" class="mt-2" />
-                            </div>
-
-                            <!-- Password -->
-                            <div>
-                                <x-input-label for="password" :value="__('Senha')" />
-
-                                <x-text-input id="password" class="block mt-1 w-full"
-                                                type="password"
-                                                name="password"
-                                                required autocomplete="new-password" />
-
-                                <x-input-error :messages="$errors->get('password')" class="mt-2" />
-                            </div>
-
-                            <!-- Confirm Password -->
-                            <div>
-                                <x-input-label for="password_confirmation" :value="__('Confirmar senha')" />
-
-                                <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                                                type="password"
-                                                name="password_confirmation" required autocomplete="new-password" />
-
-                                <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-                            </div>
-
                         </div>
 
                         <div class="flex items-center justify-end mt-4">
@@ -109,7 +51,6 @@
                                 {{ __('Salvar') }}
                             </x-primary-button>
                         </div>
-
                     </form>
                 </div>
             </div>
